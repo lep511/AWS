e0ec3e595971f6d5f8d9aba0b176c67efa43eac0
lep511 <estebanpbuday@gmail.com>
2022-12-11 10:51:15 -0300

    update

--- SAM/ArchSolution/Ejercicio02/functions/transform-data/app.py
+++ SAM/ArchSolution/Ejercicio02/functions/transform-data/app.py
@@ -1,4 +1,3 @@
-# clickstream/!{partitionKeyFromLambda:tickerSymbol}/!{partitionKeyFromLambda:year}/!{partitionKeyFromLambda:month}/
 import json
 import base64
 import logging
@@ -11,7 +10,7 @@ output = []
 
 def lambda_handler(event, context):
     
-    sample_record_to_logs = 3  # Number of records to show in logs
+    sample_records = 1  # Number of records to show in logs
     total_price = 0            # Total sum of prices in all records
     status = 'Ok'              # Status of record processing
     
@@ -22,32 +21,45 @@ def lambda_handler(event, context):
         try:
             payload = base64.b64decode(record['data']).decode('utf-8')
             json_value = json.loads(payload)
-            if sample_record_to_logs != 0:
-                logger.info("Event data: {}".format(json_value))
-                sample_record_to_logs -= 1
-        
-        except Exception as e:
-            logger.error("Error decoding payload: {}".format(e))
+            if sample_records != 0: logger.info("Original data: {}".format(json_value))
+            total_price += json_value["price"]
+            
+        except:
+            logger.error("Error in this record: {}".format(record))
             status = 'ProcessingFailed'
             continue
         
         else:
-            # Create output Firehose record and add modified payload and record ID to it.
-            firehose_record_output = {}
+            # Change format date to ISO 8601
+            new_date_sold_s = date_isoformat(json_value["dateSoldSince"])
+            new_date_sold_u = date_isoformat(json_value["dateSoldUntil"])
             
-            try:
-                total_price += json_value["PRICE"]
+            json_value["dateSoldSince"] = new_date_sold_s
+            json_value["dateSoldUntil"] = new_date_sold_u
             
-            except:
-                logger.error("Error in this record: {}".format(e))
-                status = "ProcessingFailed"    
-
+            new_payload = base64.b64encode(str(json_value).encode('utf-8'))
+            
+            if sample_records != 0:
+                logger.info("New data: {}".format(json_value))
+                sample_records -= 1
+                    
         output_record = {
             'recordId': record['recordId'],
             'result': status,
-            'data': record['data']
+            'data': new_payload
         }
         output.append(output_record)
 
     logger.info("Total price: {}".format(total_price))
     return {'records': output}
+
+   
+def date_isoformat(date_str):
+    """
+    Change format from: 'Wed Aug 26 2020 00:00:00 GMT+0000 (Some region)'
+    to '2020-08-26T00:00:00.000Z'
+    """
+    date_to_convert = date_str.split(" (")[0]
+    date_to_convert = datetime.strptime(date_to_convert, "%a %b %d %Y %H:%M:%S %Z%z")
+    date_to_convert = date_to_convert.utcnow().isoformat()
+    return date_to_convert
