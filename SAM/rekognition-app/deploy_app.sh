f5fc64566c5af6e8bf065d6d467047ea1b3fe838
lep511 <estebanpbuday@gmail.com>
2022-12-21 13:42:54 -0300

    update

--- SAM/rekognition-app/deploy_app.sh
+++ SAM/rekognition-app/deploy_app.sh
@@ -1,3 +1,6 @@
-#pip3 install -r requirements.txt -t libs/python
+# Check if a directory exists
+if [ ! -d "libs/python" ]; then
+    pip3 install -r requirements.txt -t libs/python
+fi
 sam build
 sam deploy --template-file template.yaml --capabilities CAPABILITY_NAMED_IAM --guided
