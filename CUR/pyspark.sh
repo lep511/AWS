3a7d52e9ebb5822bd17853923486c29d4b080d55
Esteban Perez <46011885+lep511@users.noreply.github.com>
2023-01-04 15:06:52 +0000

    update CUR

--- CUR/pyspark.sh
+++ CUR/pyspark.sh
@@ -5,9 +5,9 @@ sudo apt install default-jre -y
 pip install --upgrade pip -q
 
 pip install --upgrade boto3
-pip install pyspark
-pip install pandas
-pip install PyArrow 
+pip install --upgrade pyspark
+pip install --upgrade pandas
+pip install --upgrade PyArrow 
 
 export JAVA_HOME="/usr"
 export PYARROW_IGNORE_TIMEZONE=1
