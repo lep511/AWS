56322eeebebb5a011b8fdfb6993db0a831adf603
lep511 <estebanpbuday@gmail.com>
2023-12-09 08:08:14 -0300

    update

---
+++ CLI/data.py
@@ -0,0 +1,31 @@
+import boto3
+
+# Create a dynamo table
+dyn_client = boto3.client('dynamodb')
+dyn_client.create_table(
+    TableName='test_table',
+    KeySchema=[
+        {
+            'AttributeName': 'id',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'name',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'id',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'name',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 1,
+        'WriteCapacityUnits': 1
+    }
+)
