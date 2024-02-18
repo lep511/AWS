116687e96ce46cb0159d21aab53332f9d7076ad2
lep511 <estebanpbuday@gmail.com>
2024-02-18 14:15:57 -0300

    update ecs/telegram-bot

--- ECS/telegram-bot/bot.py
+++ ECS/telegram-bot/bot.py
@@ -2,7 +2,19 @@ import google.generativeai as genai
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 import json
+import requests
+from pathlib import Path
 import os
+import logging
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 API_KEY = os.environ.get('G_TOKEN')
 token = os.environ.get('T_TOKEN')
@@ -38,8 +50,33 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     text_response_r = chat_gpt(update.message.text)
     await update.message.reply_text(text_response_r, parse_mode="markdown")
-
-
+    
+async def image_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro-vision-latest")
+    
+    user = update.message.from_user
+    photo_name = "telegram-bot-image.jpeg"
+    photo_file = await update.message.photo[-1].get_file()
+    await photo_file.download_to_drive(photo_name)
+    logger.info("Photo of %s: %s", user.first_name, photo_name)
+    
+    image_parts = [
+        {
+            "mime_type": "image/jpeg",
+            "data": Path(photo_name).read_bytes()
+        }
+    ]
+    
+    prompt_parts = [
+        "Extract the author and title of the book:\n",
+        image_parts[0]
+    ]
+    
+    response = model.generate_content(prompt_parts)    
+    await update.message.reply_text(response.text)
+  
+    
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
@@ -51,6 +88,8 @@ def main() -> None:
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    
+    application.add_handler(MessageHandler(filters.PHOTO, image_handler))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
