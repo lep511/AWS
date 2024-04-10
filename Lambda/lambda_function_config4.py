dc78ec169e16ad139c9900d4af3ecac70bbc9b99
lep511 <estebanpbuday@gmail.com>
2024-04-10 16:30:33 -0300

    update

---
+++ Lambda/lambda_function_config4.py
@@ -0,0 +1,153 @@
+"""
+Función de ejemplo para evaluaciones activadas por cambios de configuración
+
+AWS Config invoca una función, como en el siguiente ejemplo, cuando detecta un cambio de configuración en un recurso 
+que se encuentra dentro del ámbito de una regla personalizada.
+
+Si utiliza la consola de AWS Config para crear una regla que esté asociada a una función como la de este ejemplo, 
+elija Configuration changes (Cambios de configuración) como tipo de disparador. Si utiliza la API de AWS Config o AWS CLI para crear la regla, 
+establezca el atributo MessageType en ConfigurationItemChangeNotification y OversizedConfigurationItemChangeNotification. 
+Estos ajustes permiten activar la regla cuando AWS Config genera un elemento de configuración o un elemento de configuración sobredimensionado 
+como resultado de un cambio de recurso.
+"""
+
+import botocore 
+import boto3
+import json
+import datetime
+
+# Set to True to get the lambda to assume the Role attached on the Config Service (useful for cross-account).
+ASSUME_ROLE_MODE = False
+
+# This gets the client after assuming the Config service role
+# either in the same AWS account or cross-account.
+def get_client(service, event):
+    """Return the service boto client. It should be used instead of directly calling the client.
+    Keyword arguments:
+    service -- the service name used for calling the boto.client()
+    event -- the event variable given in the lambda handler
+    """
+    if not ASSUME_ROLE_MODE:
+        return boto3.client(service)
+    credentials = get_assume_role_credentials(event["executionRoleArn"])
+    return boto3.client(service, aws_access_key_id=credentials['AccessKeyId'],
+                        aws_secret_access_key=credentials['SecretAccessKey'],
+                        aws_session_token=credentials['SessionToken']
+                       )
+
+# Helper function used to validate input
+def check_defined(reference, reference_name):
+    if not reference:
+        raise Exception('Error: ', reference_name, 'is not defined')
+    return reference
+
+# Check whether the message is OversizedConfigurationItemChangeNotification or not
+def is_oversized_changed_notification(message_type):
+    check_defined(message_type, 'messageType')
+    return message_type == 'OversizedConfigurationItemChangeNotification'
+
+# Get configurationItem using getResourceConfigHistory API
+# in case of OversizedConfigurationItemChangeNotification
+def get_configuration(resource_type, resource_id, configuration_capture_time):
+    result = AWS_CONFIG_CLIENT.get_resource_config_history(
+        resourceType=resource_type,
+        resourceId=resource_id,
+        laterTime=configuration_capture_time,
+        limit=1)
+    configurationItem = result['configurationItems'][0]
+    return convert_api_configuration(configurationItem)
+
+# Convert from the API model to the original invocation model
+def convert_api_configuration(configurationItem):
+    for k, v in configurationItem.items():
+        if isinstance(v, datetime.datetime):
+            configurationItem[k] = str(v)
+    configurationItem['awsAccountId'] = configurationItem['accountId']
+    configurationItem['ARN'] = configurationItem['arn']
+    configurationItem['configurationStateMd5Hash'] = configurationItem['configurationItemMD5Hash']
+    configurationItem['configurationItemVersion'] = configurationItem['version']
+    configurationItem['configuration'] = json.loads(configurationItem['configuration'])
+    if 'relationships' in configurationItem:
+        for i in range(len(configurationItem['relationships'])):
+            configurationItem['relationships'][i]['name'] = configurationItem['relationships'][i]['relationshipName']
+    return configurationItem
+
+# Based on the type of message get the configuration item
+# either from configurationItem in the invoking event
+# or using the getResourceConfigHistory API in getConfiguration function.
+def get_configuration_item(invokingEvent):
+    check_defined(invokingEvent, 'invokingEvent')
+    if is_oversized_changed_notification(invokingEvent['messageType']):
+        configurationItemSummary = check_defined(invokingEvent['configurationItemSummary'], 'configurationItemSummary')
+        return get_configuration(configurationItemSummary['resourceType'], configurationItemSummary['resourceId'], configurationItemSummary['configurationItemCaptureTime'])
+    return check_defined(invokingEvent['configurationItem'], 'configurationItem')
+
+# Check whether the resource has been deleted. If it has, then the evaluation is unnecessary.
+def is_applicable(configurationItem, event):
+    try:
+        check_defined(configurationItem, 'configurationItem')
+        check_defined(event, 'event')
+    except:
+        return True
+    status = configurationItem['configurationItemStatus']
+    eventLeftScope = event['eventLeftScope']
+    if status == 'ResourceDeleted':
+        print("Resource Deleted, setting Compliance Status to NOT_APPLICABLE.")
+    return (status == 'OK' or status == 'ResourceDiscovered') and not eventLeftScope
+
+def get_assume_role_credentials(role_arn):
+    sts_client = boto3.client('sts')
+    try:
+        assume_role_response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName="configLambdaExecution")
+        return assume_role_response['Credentials']
+    except botocore.exceptions.ClientError as ex:
+        # Scrub error message for any internal account info leaks
+        if 'AccessDenied' in ex.response['Error']['Code']:
+            ex.response['Error']['Message'] = "AWS Config does not have permission to assume the IAM role."
+        else:
+            ex.response['Error']['Message'] = "InternalError"
+            ex.response['Error']['Code'] = "InternalError"
+        raise ex
+
+def evaluate_change_notification_compliance(configuration_item, rule_parameters):
+    check_defined(configuration_item, 'configuration_item')
+    check_defined(configuration_item['configuration'], 'configuration_item[\'configuration\']')
+    if rule_parameters:
+        check_defined(rule_parameters, 'rule_parameters')
+
+    if (configuration_item['resourceType'] != 'AWS::EC2::Instance'):
+        return 'NOT_APPLICABLE'
+
+    elif rule_parameters.get('desiredInstanceType'):
+        if (configuration_item['configuration']['instanceType'] in rule_parameters['desiredInstanceType']):
+            return 'COMPLIANT'
+    return 'NON_COMPLIANT'
+
+def lambda_handler(event, context):
+
+    global AWS_CONFIG_CLIENT
+
+    check_defined(event, 'event')
+    invoking_event = json.loads(event['invokingEvent'])
+    rule_parameters = {}
+    if 'ruleParameters' in event:
+        rule_parameters = json.loads(event['ruleParameters'])
+
+    compliance_value = 'NOT_APPLICABLE'
+
+    AWS_CONFIG_CLIENT = get_client('config', event)
+    configuration_item = get_configuration_item(invoking_event)
+    if is_applicable(configuration_item, event):
+        compliance_value = evaluate_change_notification_compliance(
+                configuration_item, rule_parameters)
+
+    response = AWS_CONFIG_CLIENT.put_evaluations(
+       Evaluations=[
+           {
+               'ComplianceResourceType': invoking_event['configurationItem']['resourceType'],
+               'ComplianceResourceId': invoking_event['configurationItem']['resourceId'],
+               'ComplianceType': compliance_value,
+               'OrderingTimestamp': invoking_event['configurationItem']['configurationItemCaptureTime']
+           },
+       ],
+       ResultToken=event['resultToken'])
